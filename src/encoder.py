94e533719d34fe0b8c7c90dcfd33c237dc91d2ee
Rocco Febbo <febbo87@gmail.com>
2022-10-03 10:16:08 -0400

    Added more comments

--- src/encoder.py
+++ src/encoder.py
@@ -1,28 +1,31 @@
 # rate encoder
-#   higher values into the network -> more active network (since positive wieght values only)
-#   lower values into the network -> less active network
+#   higher values into the encoder -> faster encoder spiking -> more active network (since positive wieght values only)
+#   lower values into the encoder -> slower encoder spiking -> less active network
 class Encoder:
     def __init__(self, min_f, max_f, sim_f):
         self.last_fire = 0
 
-        # min, max, and simulation frequencies that can be represented
+        # min, max frequencies that can be output by this encoder
         self.min_f = min_f
         self.max_f = max_f
+
+        # the frequency at which the simulation should be running
         self.sim_f = sim_f
 
-        
+    # set this to whatever value the encoder should be representing
     def set_value(self, value):
+        self.last_fire = 0
+        
         # using the min and max frequency, calculate the frequency of the value
         self.value = value
         self.value_f = ((self.max_f - self.min_f) * value) + self.min_f
 
         # using the simulation frequency, calculate the period of the value
-        #   (the number of simulation time steps between fires)
+        #   (the number of simulation timesteps between fires)
         self.fire_period = self.sim_f / self.value_f
-        
-        # a potentialy faster, but memory hungry way to implement would be a look up table
 
     def update(self):
+        # step one timestep into simulation (if sim_f is 1000 Hz, one timestep is 1ms)
         self.last_fire += 1
 
         # fire everytime the fire period is completed
