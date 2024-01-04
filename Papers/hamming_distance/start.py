42cd613f009cf689f89261c4aff7c079345ba147
Rocco Febbo <febbo87@gmail.com.com>
2024-01-04 14:29:03 +0100

    parser

--- Papers/hamming_distance/start.py
+++ Papers/hamming_distance/start.py
@@ -13,19 +13,19 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Simulates SNNs as reservoirs to determine its response to different inputs using Hamming Distance')
     parser.add_argument('save_folder', type=str,
                         help='folder to save the data')
-    parser.add_argument('connectivities_min', type=float, default=0.01,
+    parser.add_argument('connectivities_min', type=float, default=0.01, required=False,
                         help='start of connectivities to test [0-1]')
-    parser.add_argument('connectivities_max', type=float, default=0.2,
+    parser.add_argument('connectivities_max', type=float, default=0.2, required=False,
                         help='end of connectivities to test [0-1]')
-    parser.add_argument('connectivities_step', type=float, default=0.01,
+    parser.add_argument('connectivities_step', type=float, default=0.01, required=False,
                         help='step of connectivities to test [0-1]')
-    parser.add_argument('n_enc', default=5, type=int,
+    parser.add_argument('n_enc', default=5, type=int, required=False,
                         help='number of encoders for the reservoir, computation time inceases exponentially with this (default: 5)')
-    parser.add_argument('n_neurons', default=100, type=int,
+    parser.add_argument('n_neurons', default=100, type=int, required=False,
                         help='number of neurons for the reservoir (default: 100)')
-    parser.add_argument('n_runs', default=100, type=int,
+    parser.add_argument('n_runs', default=100, type=int, required=False,
                         help='number of runs to perform for each connectivity (default: 100)')
-    parser.add_argument('plot', default=False, type=bool,
+    parser.add_argument('plot', default=False, type=bool, required=False,
                         help='whether or not to plot (default: False)')
 
     args = parser.parse_args()
